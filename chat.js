a45e5c65dd1662e46312d05c7cb4a518330dc5c4
Cipher <cipher@ciphers-mini.home>
2023-07-17 14:30:35 +0100

    Initial Commit

---
+++ chat.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+const IntentMatcher = require("./Janex.js");
+
+const intentsFilePath = "./intents.json";
+const matcher = new IntentMatcher(intentsFilePath);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("You: ", (input) => {
+
+  // Close the readline interface
+  rl.close();
+
+  const intentClass = matcher.patternCompare(input);
+  const response = matcher.responseCompare(input, intentClass);
+  console.log(response);
+});
